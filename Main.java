efa9976cf835bf09755e2b73ba7e383fcad242bd
Samuel König de Oliveira <skoliveira@users.noreply.github.com>
2026-07-27 19:19:10 +0000

    Nível Novato: Ambiente, Classes e Objetos

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Instanciando dois personagens
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem heroi2 = new Personagem("Arthemis", "Arqueira", 5, 80, 12.5);
 
-        .
-        .
-        .
-
+        // Chamando método exibirStatus() para cada personagem
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -5,20 +5,22 @@ public class Personagem {
     int pontosDeVida;
     double poderBase;
 
+    // Construtor da classe Personagem
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
+    // Método exibirStatus()
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Classe: " + this.classe);
+        System.out.println("Nível: " + this.nivel);
+        System.out.println("Pontos de Vida: " + this.pontosDeVida);
+        System.out.println("Poder Base: " + this.poderBase);
     }
 }
-
